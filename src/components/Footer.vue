c8473acc59372eadcc9479f9bfa02f62c35277e2
薰风习习 <1255104596@qq.com>
2022-11-28 17:30:56 +0800

    1

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,7 @@
 <template>
   <a-layout-footer style="text-align: center">
     <a-back-top />
-    <span>Copyright &copy; 2022 <a target="_blank" href="https://github.com/ikxin/kms-tools">KMS Tools</a> | </span>
-    <span>Code with by <a target="_blank" href="https://www.ikxin.com">一纸忘忧</a></span>
+    <span>Code with by <a target="_blank" href="https://blog.aidengrong">番茄酱</a></span>
     <span v-if="beian">
       | <a target="_blank" href="http://beian.miit.gov.cn/">{{ beian }}</a></span
     >
